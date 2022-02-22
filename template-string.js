da3d9338ab687aec5d4248c43c5ebecd9fc5577c
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-02-22 23:40:00 +0600

    template string

---
+++ template-string.js
@@ -0,0 +1,13 @@
+const myString = `Hello everyone.
+    My name is Mamun. 
+    Today i write ES6 code.`
+
+const myName = "Abdullah Al Mamun";
+const country = "Bangladesh";
+const profession = "Software Engineer";
+
+const mySelf = `Hello!!
+My name is: ${myName}.
+I live in ${country}.
+I am a ${profession}.`
+console.log(mySelf);  
