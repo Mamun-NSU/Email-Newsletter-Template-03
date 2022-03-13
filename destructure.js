8751c11c8f5dc93e2651409bb86b6c941baa8d7c
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-03-13 23:55:58 +0600

    destructuring

---
+++ destructure.js
@@ -0,0 +1,13 @@
+
+const person = {
+    name: 'Hamim',
+    age: 24,
+    occupation: 'Student',
+    home: 'Dhaka'
+
+};
+
+console.log(person);
+
+const { name, home } = person;
+console.log(name, home);
