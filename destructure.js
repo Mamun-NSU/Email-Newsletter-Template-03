101f32bb3e01595adc6a476fccfaf53a5308e904
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-07-14 23:54:12 +0600

    Modified successfully

--- destructure.js
+++ destructure.js
@@ -3,9 +3,10 @@ const person = {
     name: 'Hamim',
     age: 24,
     occupation: 'Student',
-    home: 'Dhaka'
+    home: 'Dhaka',
+    phone: 01711000000
 
 };
 console.log(person);
-const { name, home, age } = person;
+const { name, home, age, phone } = person;
 console.log(age, home);
