f21ba54f26c7d23cc2b04c34c237f0752a21c977
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-07-15 23:17:54 +0600

    Modified successfully

--- spread-operator.js
+++ spread-operator.js
@@ -1,5 +1,5 @@
 // spread operator
-const A = [12, 2, 4, 6, 32, 14, 7, 54, 32, 12, 6, 8, 3];
+const A = [12, 2, 4, 6, 32, 14, 32, 12, 6, 8, 3];
 
 console.log(...A);
 console.log("Max number is:", Math.max(...A));
